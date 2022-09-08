57ff4baaeee85358e5f50c354ea22437eedd2217
Eric Sampaio <erick.gtn.xp@gmail.com>
2022-09-08 08:21:58 -0300

    Questão 1 e 2

---
+++ Exercicios-em-python/Questão 1.py
@@ -0,0 +1,5 @@
+'1. Faça um algoritmo que receba dois números e exiba o resultado da sua soma.'
+a=int(input("Informe um valor: "))
+b=int(input("Informe outro valor: "))
+c=a+b
+print(c)
---
+++ Exercicios-em-python/Questão 2.py
@@ -0,0 +1,9 @@
+'Faça um algoritmo que receba dois números e ao final mostre a soma, subtração, multiplicação e a divisão dos números lidos.'
+
+a=int(input("Informe um valor: "))
+b=int(input("Informe outro valor: "))
+
+print("Soma: ",a+b)
+print("Subtração: ",a-b)
+print("Divisão: ",a/b)
+print("Multiplicação: ",a*b)

5aee320269067a7e971466da10f54b34a7582a18
Eric Sampaio <erick.gtn.xp@gmail.com>
2022-09-10 22:45:30 -0300

    Questões 6, 7 e 8

---
+++ Exercicios-em-python/Questão 6.py
@@ -0,0 +1,9 @@
+'Ler dois valores para as variáveis A e B, e efetuar as trocas dos valores de forma que a variável A passe a possuir o valor da variável B e a variável B passe a possuir o valor da variável A. Apresentar os valores trocados.'
+
+A=input()
+B=input()
+C=A
+A=B
+B=C
+print(A)
+print(B)
---
+++ Exercicios-em-python/Questão 7.py
@@ -0,0 +1,5 @@
+'7. Ler uma temperatura em graus Celsius e apresentá-la convertida em graus Fahrenheit. A fórmula de conversão é: F=(9*C+160) / 5, sendo F a temperatura em Fahrenheit e C a temperatura em Celsius.'
+
+celcius=float(input("Informe a temperatura em graus Celcius: "))
+fahrenheit = (9*celcius+160)/5
+print(" Em Fahenheit: ",fahrenheit)

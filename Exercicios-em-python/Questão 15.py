0a44973c147378f5648dc9da8fedf423e8ef8968
Eric Sampaio <erick.gtn.xp@gmail.com>
2022-09-12 21:53:27 -0300

    Questão 15

---
+++ Exercicios-em-python/Questão 15.py
@@ -0,0 +1,10 @@
+'Estrutura de decisão'
+'15. Faça um algoritmo que receba um número e diga se este número está no intervalo entre 100 e 200.'
+
+
+num = int(input("Informe um número:\n"))
+if(num > 100 and num < 200):
+  print(" Número pertence ao intervalo de 100 a 200")
+else:
+  print(" Número não pertence ao intervalo de 100 a 200")
+  

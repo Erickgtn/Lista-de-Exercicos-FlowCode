e48ab514fc6a82e7e28e1b3a013a2a3b7aa6f85f
Eric Sampaio <erick.gtn.xp@gmail.com>
2022-09-12 21:51:40 -0300

    Questão 13

---
+++ Exercicios-em-python/Questão 13.py
@@ -0,0 +1,10 @@
+'Estrutura de decisão'
+'13. Faça um algoritmo que receba um número e mostre uma mensagem caso este número seja maior que 10.'
+
+num = int(input("Informe um número: "))
+if(num > 10):
+  print("Maior que 10")
+elif(num==10):
+  print("Número é 10")
+else:
+  print("Menor que 10")

c0413dae14ef81108036b0bae0dd4c2857c6d4d4
Eric Sampaio <erick.gtn.xp@gmail.com>
2022-09-12 21:52:34 -0300

    Questão 14

---
+++ Exercicios-em-python/Questão 14.py
@@ -0,0 +1,12 @@
+'Estrutura de decisão'
+'14. Escrever um algoritmo que leia dois valores inteiro distintos e informe qual é o maior.'
+
+A = int(input("Informe um valor: "))
+B = int(input("Informe outro valor: "))
+
+if (A>B):
+  print(A,"É maior que: ",B)
+else:
+    print(B,"É maior que: ",A)
+
+  

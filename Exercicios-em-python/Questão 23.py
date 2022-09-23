1f63d5922e0df2175ec1c508429bc2384479437a
Eric Sampaio <erick.gtn.xp@gmail.com>
2022-09-23 11:31:37 -0300

    Questão 23

---
+++ Exercicios-em-python/Questão 23.py
@@ -0,0 +1,12 @@
+'Decisão'
+'23. Faça um algoritmo que receba um número e mostre uma mensagem caso este número seja maior que 80, menor que 25 ou igual a 40.'
+
+
+numero=int(input("Informe um número: "))
+
+if (numero > 80 ):
+  print("Numero maior que 80")
+elif (numero < 25):
+  print("Numero menor que 25!")
+elif(numero == 40):
+  print("Número igual a 40!")
